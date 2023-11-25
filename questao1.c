21c9003fcae7dca1aafe57721aa13db6638d1965
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-25 18:40:47 -0300

    Update questao1.c

--- questao1.c
+++ questao1.c
@@ -1,24 +1,24 @@
-//Exerc�cio 1: 
+//Exercício 1: 
 //Crie um algoritmo que leia o nome, a idade, o peso e a altura de 5 pessoas, armazenando em vetores.
 
-// Mostre as informa��es solicitadas de cada pessoa e informe:
+// Mostre as informações solicitadas de cada pessoa e informe:
 //O nome e a altura da pessoa mais alta e a mais baixa.
 //O nome e o peso da pessoa com maior peso e menor peso;
 //O nome a idade da pessoa com mais idade e com menos idade.
 #include<stdio.h>
 int main(){
-float altura[5];
-float maioraltura[5];
-float menoraltura[5];
-float peso[5];
-float maiorpeso[5];
-float menorpeso[5];
-int maisidade;
-int menosidade;
-int i,j;
-char nome[200][5];
-int idade[5];
-for ( i = 0; i < 5; i++)
+float altura[2];
+float maioraltura[2];
+float menoraltura[2];
+float peso[2];
+float maiorpeso[2];
+float menorpeso[2];
+int maisidade[2];
+int menosidade[2];
+int i;
+char nome[200][2];
+int idade[2];
+for ( i = 0; i < 2; i++)
 {
 printf("Digite seu nome:\n");
 scanf("%s",&nome[i]);
@@ -29,7 +29,38 @@ scanf("%f",&altura[i]);
 printf("Digite seu peso:\n");
 scanf("%f",&peso[i]);
 }
+printf("Exibindo Informacoes:\n");
 if(maioraltura){
+printf("A Pessoa com maior altura é:\n");
+printf("nome:%d\n",nome[i]);
+printf("maioraltura:%d\n",maioraltura[i]);
+}
+if(menoraltura){
+printf("A Pessoa com menor altura é:\n");
+printf("nome:%d\n",nome[i]);
+printf("menoraltura:%d\n",menoraltura[i]);
+}
+if(maisidade){
+printf("A Pessoa com mais idade é:\n");
+printf("nome:%d\n",nome[i]);
+printf("maisidade:%d\n",maisidade[i]);
+
+if(menosidade){
+printf("A Pessoa com menos idade é:\n");
+printf("nome:%d\n",nome[i]);
+printf("menosidade:%d\n",menosidade[i]);
+
+if(maiorpeso){
+printf("A Pessoa com mais peso:\n");
+printf("nome:%d\n",nome[i]);
+printf("maiorpeso:%d\n",maiorpeso[i]);
+}
+if(menorpeso){
+printf("A Pessoa com menor peso\n");
+printf("nome:%d\n",nome[i]);
+printf("menorpeso:%d\n",menorpeso[i]);
+}
+}
 
 }
 return 0;
