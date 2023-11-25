179dfc88fd6d60c73531fc9c7a65d7c5d2ed0984
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-25 18:43:35 -0300

    Update questao4.c

--- questao4.c
+++ questao4.c
@@ -1,33 +1,73 @@
-//Exercício 4: 
-//Crie um software que leia 5 números inteiros e, em seguida, mostre na tela:
-//A quantidade de números ímpares;
-//A quantidade de números negativos;
-//O maior e o menor número;
-//A média de números pares;
-//A média de números inseridos.
-int i,j;
-int numerosnegativos[5];
-int impares[5];
-int pares[5];
-float mediaimpares=0;
-float mediapares=0;
-int numero[5];
-int negativos;
-int somapares;
-for( i = 0; i <5; i++)
+#include <stdio.h>
+//ExercÃ­cio 4: 
+//Crie um software que leia 5 nÃºmeros inteiros e, em seguida, mostre na tela:
+//A quantidade de nÃºmeros Ã­mpares;
+//A quantidade de nÃºmeros negativos;
+//O maior e o menor nÃºmero;
+//A mÃ©dia de nÃºmeros pares;
+//A mÃ©dia de nÃºmeros inseridos.
+int main()
 {
-printf("Digite 5 números:");
-scanf("%d",&numero);  
-}
-if(numero[i]%2==0){
-pares=pares+1;
-somapares=somapares+numeros[i];
-}
-{
-{
-else
-impares[i]++;
-mediaimpares=impares/2;
-}
-return 0;
+    int numeros[5];
+    int i;
+    int pares = 0;
+    int impares = 0;
+    int negativos = 0;
+    int maior = 0;
+    int menor = 9999;
+    int somaPares = 0;
+    float mediaPares = 0;
+    int somatodosnumeros = 0;
+    float mediatodosnumeros=0;
+
+    for (i = 0; i < 5; i++) {
+        printf("Digite um nÃºmero: ");
+        scanf("%d", &numeros[i]);
+
+        if (numeros[i] % 2 == 0) {
+            pares = pares + 1;
+            somaPares = somaPares + numeros[i];
+        } else {
+            impares++;
+        }
+
+        {
+        if (numeros[i] < 0) {
+            negativos++;
+        }
+        {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+        }
+        }
+        if (numeros[i] < menor) {
+            menor = numeros[i];     
+        }
+
+       
+        somatodosnumeros = somatodosnumeros + numeros[i];
+    }
+
+    
+    if (pares > 0) {
+        mediaPares = somaPares / (float) pares;
+    }
+
+    mediatodosnumeros = somatodosnumeros / (float) i;
+    printf("\nExibindo os valores:\n");
+    printf("\nQuantidade de numeros pares: %d \n", pares);
+    printf("Quantidade de numeros Ã­mpares: %d \n", impares);
+    printf("Quantidade de numeros negativos: %d \n", negativos);
+    printf("Quantidade de numeros inseridos: %d \n", i);
+    printf("Maior numero: %d \n", maior);
+    printf("Menor numero: %d \n", menor);
+    printf("Media de numeros pares: %.1f \n", mediaPares);
+    printf("Media de todos os numeros inseridos: %.1f \n", mediatodosnumeros);;
+    }
+
+    {
+        
+    }
+
+    return 0;
 }
