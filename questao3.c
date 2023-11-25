2795fb38c44b512ff1c74dd65e1ed9a41c2cda12
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-25 18:42:09 -0300

    Update questao3.c

--- questao3.c
+++ questao3.c
@@ -1,30 +1,42 @@
-//Exercício 3: 
-//Implemente um programa que permita cadastrar contatos telefônicos em uma agenda, 
-//utilizando vetores para armazenar os nomes e números de telefone. 
-//Permita que o usuário digite o código 1 para adicionar um número e 2 para mostrar os números cadastrados e encerrar o software.
-#include<stdio.h>
-int main(){
-int numerotelefone[2];
-char nome[200][2];
-int i,j;
-for ( i = 0; i <2; i++)
+// ExercÃ­cio 3:
+// Implemente um programa que permita cadastrar contatos telefÃ´nicos em uma agenda,
+// utilizando vetores para armazenar os nomes e nÃºmeros de telefone.
+// Permita que o usuÃ¡rio digite o cÃ³digo 1 para adicionar
+// um nÃºmero e 2 para mostrar os nÃºmeros cadastrados e encerrar o software.
+#include <stdio.h>
+#include<locale.h>
+int main()
 {
-printf("Digite um numero de telefone:");
-scanf("%d",&numerotelefone[i]);  
-}
-switch (numerotelefone[i])
-{
-    fflush(stdin);
-case 1:
-    printf("1-Adicionar numero");
-    scanf("%s",&numerotelefone);
-    printf("Digite o numero 1");
-    break;
-case 2:
-printf("2-Mostrar numeros cadastrados e encerrar o software");
-printf("Digite o numero 2");
-    break;
-}
-return 0;
-}
+    setlocale(LC_ALL,"portuguese");
+    int numerotelefone[2];
+    char nome[200][2];
+    int opcao;
+    int i;
+    
+            printf("Digite 1 ou 2:\n");
+            printf("1-Adicionar numero\n");
+            printf("2-Sair e mostrar resultados\n");
+            printf("Digite a opÃ§Ã£o desejada:");
+            scanf("%d",&opcao);
+            switch (opcao)
+            {
+            case 1:
+            printf("====Digite um numero de telefone==\n");
+            scanf("%d",&numerotelefone);
+                printf("\n\nDigite um nome:");
+                scanf("%d", &nome[i]);
+                fflush(stdin);
+                break;
+            case 2:
+                printf("Saindo do programa:\n");
+                break;
+                default:
+                printf("Opcao Invalida\n");
+                printf("numerotelefone:%d\n",numerotelefone);
+                printf("nome:%d\n",nome);
+        
+        
+    }
+        return 0;
+    }
 
